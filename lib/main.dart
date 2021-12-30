15eb4106237311d4f64eb2a4dae9de41866a690b
baborges45 <brunoquemsoueu@gmail.com>
2021-12-30 20:31:45 -0300

    creating the service that will be responsible for storing the StorageService data and the Task class that will have the data for each task

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
+import 'package:todo_list_flutter/app/data/services/services.dart';
 
 import 'app/modules/home/view.dart';
 
 void main() async {
   await GetStorage.init();
+  await Get.putAsync(() => StorageService().init());
   runApp(const MyApp());
 }
 
