9e0a3847468875d382c1e73f936b751a2782392d
baborges45 <brunoquemsoueu@gmail.com>
2021-12-30 16:35:39 -0300

    creating structure colors and icons

---
+++ lib/app/core/values/icons.dart
@@ -0,0 +1,17 @@
+///person
+const personIcon = 0xe491;
+
+///work
+const workIcon = 0xe11c;
+
+///movie
+const movieIcon = 0xe40f;
+
+///sport
+const sportIcon = 0xe4dc;
+
+///travel
+const travelIcon = 0xe071;
+
+///shop
+const shopIcon = 0xe59c;
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 
 import 'app/modules/home/view.dart';
 
-void main() {
+void main() async {
+  await GetStorage.init();
   runApp(const MyApp());
 }
 
